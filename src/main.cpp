6d991228fb1a0c42a81daf4f56a1c484d20c382c
Mystical :D <peterpanlee2015@gmail.com>
2025-04-30 22:46:04 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
-// for silly bon
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GameManager.hpp>
-
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, GameManager) {
-    bool isIconUnlocked(int p0, IconType p1) {
-        bool result = GameManager::isIconUnlocked(p0, p1);
-
-        if (Mod::get()->getSettingValue<bool>("icon-hack")) {
-            return true;
-        }
-
-        return result;
+class $modify(GameManager) {
+    bool isIconUnlocked(int _id, IconType _type) {
+        if (GameManager::isIconUnlocked(_id, _type)) return true;
+        if (_id <= 0) return false;
+        //if (_id >= GameManager::countForType(_type)) return false;
+        return Hacks::isHackEnabled("Icon Bypass");
+    }
+    bool isColorUnlocked(int _id, UnlockType _type) {
+        if (GameManager::isColorUnlocked(_id, _type)) return true;
+        return Hacks::isHackEnabled("Icon Bypass");
     }
 };
