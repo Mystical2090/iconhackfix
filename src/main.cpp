f394a92fd8f63205477aba364733de7b183623a3
Mystical :D <peterpanlee2015@gmail.com>
2025-04-30 22:33:41 +0900

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+// for silly bon
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GameManager.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyMenuLayer, GameManager) {
+    bool isIconUnlocked(int p0, IconType p1) {
+        bool result = GameManager::isIconUnlocked(p0, p1);
+
+        if (Mod::get()->getSettingValue<bool>("icon-hack")) {
+            return true;
+        }
+
+        return result;
+    }
+};
